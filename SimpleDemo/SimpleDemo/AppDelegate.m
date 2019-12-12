7e736b45ed90c0a936331c876d5f89f7837437f6
zhangze <zhangze@wanbu.com.cn>
2019-12-12 15:57:25 +0800

    运行配置参数

--- SimpleDemo/SimpleDemo/AppDelegate.m
+++ SimpleDemo/SimpleDemo/AppDelegate.m
@@ -18,7 +18,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+
+    self.window = [[UIWindow alloc] initWithFrame: [UIScreen mainScreen].bounds];
 
 
 
@@ -57,18 +58,20 @@
 
     self.window.rootViewController = navigationController;
     [self.window makeKeyAndVisible];
+
     return YES;
 }
 
 
 #pragma mark - UISceneSession lifecycle
 
-
+/*
 - (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
     // Called when a new scene session is being created.
     // Use this method to select a configuration to create the new scene with.
     return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
 }
+ */
 
 
 - (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
